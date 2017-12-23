5aa3b1edaa15253244ddb49d7fb3ab85932e8330
SuryashankarDas <suryashankardas.2002@gmail.com>
2017-12-23 20:00:19 +0530

    Revert "add dt2w for mtk"
    This reverts commit 51148d618fb3081e7c127353fec168f1d78cda44.

--- drivers/input/touchscreen/mediatek/mtk_tpd.c
+++ drivers/input/touchscreen/mediatek/mtk_tpd.c
@@ -46,14 +46,6 @@
 #define COMPAT_TPD_GET_FILTER_PARA _IOWR(TOUCH_IOC_MAGIC,2,struct tpd_filter_t) 
 #endif
 
-#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
-#ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
-#include <linux/input/sweep2wake.h>
-#endif
-#ifdef CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE
-#include <linux/input/doubletap2wake.h>
-#endif
-#endif
 
 extern int tpd_v_magnify_x;
 extern int tpd_v_magnify_y;
@@ -327,11 +319,7 @@ static int __init tpd_device_init(void);
 static void __exit tpd_device_exit(void);
 static int tpd_probe(struct platform_device *pdev);
 static int tpd_remove(struct platform_device *pdev);
-#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
-#include <cust_eint.h>
-void (*nyx_suspend) (struct early_suspend *h);
-void (*nyx_resume) (struct early_suspend *h);
-#endif
+
 #ifndef CONFIG_HAS_EARLYSUSPEND
 static int tpd_suspend_flag = 0;
 #endif
@@ -504,72 +492,6 @@ static void tpd_create_attributes(struct device *dev, struct tpd_attrs *attrs)
 		device_create_file(dev, attrs->attr[--num]);
 }
 
-#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
-
-static void eros_suspend(struct early_suspend *h) {
-#if defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE) || defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE)
-	bool prevent_sleep = false;
-#endif
-	/*
-	 * we're taking a gamble here and assuming that the suspend/resume calls will
-	 * be correctly made by the kernel everytime screen suspend/resume is made.
-	 *
-	 * If it doesn't, well, that breaks things.
-	 *
-	 */
-#if defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE)
-	prevent_sleep = (s2w_switch > 0) && (s2w_s2sonly == 0);
-	s2w_scr_suspended = true;
-#endif
-#if defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE)
-	prevent_sleep = prevent_sleep || (dt2w_switch > 0);
-	dt2w_scr_suspended = true;
-#endif
-
-	if (prevent_sleep) {
-		mt_eint_unmask(CUST_EINT_TOUCH_PANEL_NUM);
-	} else {
-		nyx_suspend(h);
-	}
-}
-
-static void eros_resume(struct early_suspend *h) {
-#if defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE) || defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE)
-	bool prevent_sleep = false;
-#endif
-#if defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE)
-	prevent_sleep = (s2w_switch > 0) && (s2w_s2sonly == 0);
-	s2w_scr_suspended = false;
-#endif
-#if defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE)
-	prevent_sleep = prevent_sleep || (dt2w_switch > 0);
-	dt2w_scr_suspended = false;
-#endif
-
-	if (prevent_sleep) {
-		mt_eint_mask(CUST_EINT_TOUCH_PANEL_NUM);
-		/*
-		 * now that we've masked back CUST_EINT_TOUCH_PANEL_NUM, a touch panel reset
-		 * needs to be called. However, since sprout has 5 different panels, just
-		 * call a suspend/resume cycle to allow a normal ctp reset.
-		 * Time constraints shouldn't be much given just the GPIO's are cleared,
-		 * however, maybe we should just call _resume alone since the ctp driver
-		 * should be able to handle cases where the panel is already in _resume
-		 * but waiting for an IRQ flush (?) or a GPIO reset(?).
-		 *
-		 * This is similar to ft5x06_ts's panel behaviour during _HIBERNATE mode
-		 * where the ctp doesn't respond to anything but hard reset calls.
-		 *
-		 */
-		nyx_suspend(h);
-		nyx_resume(h);
-	} else {
-		nyx_resume(h);
-	}
-}
-
-#endif
-
 /* touch panel probe */
 static int tpd_probe(struct platform_device *pdev)
 {
@@ -592,21 +514,6 @@ static int tpd_probe(struct platform_device *pdev)
 #endif
 #endif
 
-#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
-	if ((g_tpd_drv->suspend != NULL) && (g_tpd_drv->resume != NULL)) {
-		if ((nyx_suspend == NULL) && (nyx_resume == NULL)) {
-			/*
-			 * in case nyx_suspend || nyx_resume != NULL
-			 * they have _suspend|_resume functions in them;
-			 * in which case leave them alone.
-			 */
-			nyx_suspend = g_tpd_drv->suspend;
-			nyx_resume  = g_tpd_drv->resume;
-			g_tpd_drv->suspend = eros_suspend;
-			g_tpd_drv->resume  = eros_resume;
-		}
-	}
-#endif
 	if (misc_register(&tpd_misc_device)) {
 		printk("mtk_tpd: tpd_misc_device register failed\n");
 	}
