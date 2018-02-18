ff605df8d31dfd7ccdcd1472f5fcbc40fa7558c6
SuryashankarDas <suryashankardas.2002@gmail.com>
2018-02-18 12:15:19 +0530

    fix error

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -414,8 +414,6 @@ struct binder_transaction {
 };
 
 static void binder_defer_work(struct binder_proc *proc, enum binder_deferred_state defer);
-static inline void binder_lock(const char *tag);
-static inline void binder_unlock(const char *tag);
 static int task_get_unused_fd_flags(struct binder_proc *proc, int flags)
 {
 	struct files_struct *files = proc->files;
